55e6253593e954c61033dba1e2ea73efca7084fb
Oleg Chistov <oleguszte@gmail.com>
2023-03-01 02:03:22 +0300

    Unfinished a13 backup2

--- A13.java
+++ A13.java
@@ -6,48 +6,102 @@ import java.util.Deque;
 
 public class A13 {
 
-    public static int evaluater(String expression) {
-        Deque<Character> onesAndZeros = new ArrayDeque<>();
-        Deque<Character> functions = new ArrayDeque<>();
-        boolean wasThereAnExlMark = false;
+    public static boolean intToBool(int x) {
+        if (x == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
-        for (int i = 0; i < expression.length(); i++) {
-            char token = expression.charAt(i);
-            switch (token) {
-                case '0', '1' -> {
-                    onesAndZeros.addFirst(token);
-                }
-                case '!' -> {
-                    i++;
-                    wasThereAnExlMark = true;
-                }
-                case '(' -> {
-                    StringBuilder sb = new StringBuilder();
-                    int OBracketCOunter = 1;
-                    int CBracketCOunter = 0;
-                    i++;
-                    while (OBracketCOunter != CBracketCOunter) {
-                        if (expression.charAt(i) == '(') {
-                            OBracketCOunter++;
-                        } else if (expression.charAt(i) == ')') {
-                            CBracketCOunter++;
-                        }
+    public static int boolToInt(boolean x) {
+        if (x) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 
-                    }
-                }
+
+
+    public static int functionWeight(char ch) {
+        switch (ch) {
+            case '!' -> {return 3;}
+            case '&' -> {return 2;}
+            case '|' -> {return 1;}
+            case '^' -> {return 0;}
+            default -> {
+                throw new IllegalArgumentException("functionWeight function collapsed <- for dev");
             }
         }
     }
+
+    public static boolean isFunction(char ch) {
+        switch (ch) {
+            case '!', '|', '^', '&' -> {return true;}
+            default -> {
+                return false;
+            }
+        }
+    }
+
+    public static int evaluater(String expression) {
+        if (expression.length() == 1) {
+            return Character.valueOf(expression.charAt(0));
+        }
+        int bracketFlag = 0;
+        if (expression.length() == 2) {
+            if (expression.charAt(1) == '0') {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        if (expression.charAt(0) == '(' && expression.charAt(expression.length()-1) == ')') {
+            bracketFlag = 1;
+        }
+        int indexOfLeastWeightedFun = 0;
+        char leastWfunction = ' ';
+        int leastFunctionWeight = Integer.MAX_VALUE;
+        for (int i = expression.length()-bracketFlag-1; i >= 0; i--) {
+            if (isFunction(expression.charAt(i)) && functionWeight(expression.charAt(i)) <= leastFunctionWeight) {
+                indexOfLeastWeightedFun = i;
+                leastWfunction = expression.charAt(i);
+                leastFunctionWeight = functionWeight(expression.charAt(i));
+            }
+        }
+        final boolean rightExpressionEvaluated = intToBool(evaluater(expression.substring(indexOfLeastWeightedFun + 1, expression.length() - bracketFlag)));
+        final boolean leftExpressionEvaluated= intToBool(evaluater(expression.substring(bracketFlag, indexOfLeastWeightedFun)));
+
+        switch (leastWfunction) {
+            case '!' -> {
+                if (evaluater(expression.substring(1)) == 1) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            }
+            case '&' -> {
+                return  boolToInt(leftExpressionEvaluated && rightExpressionEvaluated);
+            }
+            case '|' -> {
+                return  boolToInt(leftExpressionEvaluated | rightExpressionEvaluated);
+            }
+            case '^' -> {
+                return  boolToInt(leftExpressionEvaluated ^ rightExpressionEvaluated);
+            }
+        }
+        return -99999999;
+
+    }
+
 
     public static void main(String[] args) {
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
             String expression = reader.readLine();
-
-
-
-
+            System.out.println(evaluater(expression));
         } catch (IOException e) {
             System.out.println("Exception during reading the input" + e.getMessage());
         }
