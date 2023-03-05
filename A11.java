4fc792edd1c888c9a86dfdd8783dc6d8cc2a4fef
Oleg Chistov <oleguszte@gmail.com>
2023-03-05 20:37:35 +0300

    A11 finished

--- A11.java
+++ A11.java
@@ -1,17 +1,21 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
 
 public class A11 {
 
-    private static float[] toFloatArr(String str) {
+    private static double[] toDoubleArr(String str) {
         String[] strsplit = str.split(" ");
 
-        float[] ret = new float[strsplit.length];
+        double[] ret = new double[strsplit.length];
 
-        for (int i = 0; i < strsplit.length; i++) {
-            ret[i] = Float.parseFloat(strsplit[i]);
+        for (int i = 1; i < strsplit.length; i++) {
+            ret[i] = Double.parseDouble(strsplit[i]);
         }
         return ret;
     }
@@ -23,8 +27,12 @@ public class A11 {
             int n = Integer.parseInt(reader.readLine());
 
             for (int i = 0; i < n; i++) {
-                float[] arrayToCheck = toFloatArr(reader.readLine());
-                System.out.println(checkerIfSortPos(arrayToCheck));
+                double[] arrayToCheck = toDoubleArr(reader.readLine());
+                if (checker(arrayToCheck)) {
+                    System.out.println(1);
+                } else {
+                    System.out.println(0);
+                }
             }
 
 
@@ -33,29 +41,31 @@ public class A11 {
         }
     }
 
-    private static int checkerIfSortPos(float[] array) {
-        float minValue = findMin(array);
+    private static boolean checker(double[] arrayToCheck) {
+        Deque<Double> stack = new ArrayDeque<>();
+        double[] sortedArray = new double[arrayToCheck.length];
+        System.arraycopy(arrayToCheck, 0, sortedArray, 0, arrayToCheck.length);
+        Arrays.sort(sortedArray);
 
-        for (int i = 0; i < array.length-1; i++) {
-            if (array[i] < array[i+1] && array[i] != minValue) {
-                return 0;
-            } else if (array[i] == minValue) {
-                minValue = findMin(Arrays.copyOfRange(array, i+1, array.length));
+        int theNextRequaired = 0;
+        for (int i = 0; i < arrayToCheck.length; i++) {
+            if (arrayToCheck[i] == sortedArray[theNextRequaired]) {
+              theNextRequaired++;
+              while (!stack.isEmpty() && stack.getFirst() == sortedArray[theNextRequaired]) {
+                  stack.removeFirst();
+                  theNextRequaired++;
+              }
+            } else {
+                stack.addFirst(arrayToCheck[i]);
             }
         }
-        return 1;
+        if (stack.isEmpty() && theNextRequaired == arrayToCheck.length) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    private static float findMin(float[] array) {
-        float min = Float.MAX_VALUE;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == 1.0) {
-                return 1;
-            } else if (min > array[i]) {
-                min = array[i];
-            }
-        }
-        return min;
-    }
+
 }
 
